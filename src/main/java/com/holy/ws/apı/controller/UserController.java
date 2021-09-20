59ceb373fa635ff7f6cba2923f7217c39f26110a
Vugar <mammadli.vugar@hotmail.com>
2021-09-20 11:14:25 +0400

    UserController.java added

---
+++ src/main/java/com/holy/ws/apı/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.holy.ws.apı.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/1.0/user")
+public class UserController {
+}
